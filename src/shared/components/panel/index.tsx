d02cc2f5f9f358f62117e3727d768b4c114d357f
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-26 12:27:36 -0600

    feat: add size support to panel component

--- src/shared/components/panel/index.tsx
+++ src/shared/components/panel/index.tsx
@@ -4,16 +4,18 @@ import './index.styles.css';
 export type PanelProps = {
   className?: string;
   children: string | number | ReactElement | Array<ReactElement>;
+  size?: 'short' | 'full';
 };
 
 export const Panel = (props: PanelProps) => {
   const {
     className = '',
     children,
+    size = 'short',
   } = props;
 
   return (
-    <div className={`panel ${className}`}>
+    <div className={`panel ${className} panel-size--${size}`}>
       {children}
     </div>
   );
