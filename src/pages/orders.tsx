88156fdfd1b2114505e2e464cd0bcb63b2b96a31
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-26 14:31:43 -0600

    feat: add OrdersListContainer to orders page

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -6,6 +6,7 @@ import { GlobalState } from '../shared/states/global';
 import { Typography } from '../shared/components/typography';
 import { IsAuthenticated } from '../elements/isAuthenticated/components';
 import { HeaderContainer } from '../elements/headers/containers';
+import { OrdersListContainer } from '../elements/ordersList/containers';
 
 export const Orders = () => {
   const navigate = useNavigate();
@@ -25,6 +26,8 @@ export const Orders = () => {
           <Typography component='h4' className='mt-0 mb-4'>
             Órdenes de Comida
           </Typography>
+
+          <OrdersListContainer />
         </IsAuthenticated>
       </Page>
     </>
